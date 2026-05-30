151ddb78406e4249b7862e1c8543b8f21051528f
XtophB <xtophb@gmail.com>
2026-05-31 01:45:54 +0200

    feat: minigame child now kills itself when time is over

--- scripts/paintingMinigame/painting_minigame_pass_controls.gd
+++ scripts/paintingMinigame/painting_minigame_pass_controls.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+signal gameOver
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,3 +14,7 @@ func _input(event: InputEvent) -> void:
 	for child in get_children():
 		if child is SubViewport:
 			child.push_input(event)
+
+func _on_node_2d_drawing_phase_over() -> void:
+	print("gameOver emitted")
+	gameOver.emit()
